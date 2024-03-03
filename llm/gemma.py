3a0d71b9b8274f886a73e29048cf74f9c5321aba
KZunT <rlawnsxo0224@naver.com>
2024-03-03 22:40:05 +0900

    google gemma 2b

---
+++ llm/gemma.py
@@ -0,0 +1,10 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
+model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it")
+
+input_text = "Write me a poem about Machine Learning."
+input_ids = tokenizer(input_text, return_tensors="pt")
+
+outputs = model.generate(**input_ids)
+print(tokenizer.decode(outputs[0]))
