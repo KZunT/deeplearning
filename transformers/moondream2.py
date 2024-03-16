fe7ad6b90ea8179dc8f1f5af23c583f50242b9ec
KZunT <rlawnsxo0224@naver.com>
2024-03-16 19:43:13 +0900

    vikhyatk moondream2 hf

---
+++ transformers/moondream2.py
@@ -0,0 +1,13 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from PIL import Image
+
+model_id = "vikhyatk/moondream2"
+revision = "2024-03-06"
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, trust_remote_code=True, revision=revision
+)
+tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
+
+image = Image.open("<IMAGE_PATH>")
+enc_image = model.encode_image(image)
+print(model.answer_question(enc_image, "Describe this image.", tokenizer))
