53ff8d662f5f0eb1e387e797493b50354ab85ac7
KZunT <rlawnsxo0224@naver.com>
2024-04-05 01:29:44 +0900

    facebook wav2vec2

---
+++ speech_to_text/wav2vec2.py
@@ -0,0 +1,22 @@
+from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
+from datasets import load_dataset
+import torch
+
+# load model and tokenizer
+processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+
+# load dummy dataset and read soundfiles
+ds = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation")
+
+# tokenize
+input_values = processor(
+    ds[0]["audio"]["array"], return_tensors="pt", padding="longest"
+).input_values  # Batch size 1
+
+# retrieve logits
+logits = model(input_values).logits
+
+# take argmax and decode
+predicted_ids = torch.argmax(logits, dim=-1)
+transcription = processor.batch_decode(predicted_ids)
