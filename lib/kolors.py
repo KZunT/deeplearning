f7838f82593c32a6e261b4da37bd1962cd46477c
KZunT <rlawnsxo0224@naver.com>
2024-07-11 20:56:27 +0900

    kolors

---
+++ lib/kolors.py
@@ -0,0 +1,10 @@
+# apt-get install git-lfs
+# git clone https://github.com/Kwai-Kolors/Kolors
+# cd Kolors
+# conda create --name kolors python=3.8
+# conda activate kolors
+# pip install -r requirements.txt
+# python3 setup.py install
+# git lfs clone https://huggingface.co/Kwai-Kolors/Kolors weights/Kolors
+
+# python3 scripts/sample.py "하얀 눈 위의 고양이"
